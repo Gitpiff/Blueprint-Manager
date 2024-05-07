2cce3bc0ba3f3895df84b3c124a7baec1539438e
Victor <viknavarro@gmail.com>
2024-05-08 00:24:59 +0100

    Middleware

--- backend/db/models/projectmanager.js
+++ backend/db/models/projectmanager.js
@@ -74,6 +74,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'ProjectManager',
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
   });
   return ProjectManager;
 };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,41 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
+
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { ProjectManager } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await ProjectManager.findOne({
+    where: {
+      username: 'Demo-Lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.projectmanager);
+    }
+);
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.projectmanager);
+  }
+);
+
 
 module.exports = router;
