7d8c23aa808c30eadc04ca6d481fa719f31b3bc1
Victor <viknavarro@gmail.com>
2024-05-07 22:25:42 +0100

    PM Model, Migration & Seeder Files

---
+++ backend/db/models/projectmanager.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class ProjectManager extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ProjectManager.init({
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.");
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    companyName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 30],
+      }
+    },
+    industrySector: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'ProjectManager',
+  });
+  return ProjectManager;
+};
---
+++ backend/db/seeders/20240507002934-demo-project-manager.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+const demoUsers = [
+  {
+    firstName: "Demo",
+    lastName: "Lition",
+    username: "Demo-Lition",
+    companyName: "Demo LLC",
+    email: "demo@test.io",
+    industrySector: "Demolition",
+    hashedPassword: bcrypt.hashSync('password')
+  },
+  {
+    firstName: "Jose",
+    lastName: "Perez",
+    username: "joseperez",
+    companyName: "Perez LLC",
+    email: "jose@test.io",
+    industrySector: "Construction",
+    hashedPassword: bcrypt.hashSync('password2')
+  }
+];
+
+options.tableName = 'ProjectManagers';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   try {
+     await queryInterface.bulkInsert(options, demoUsers)
+   } catch (error) {
+    console.log(`Error in bulk inserting PMs: `, error)
+   }
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    try {
+      const Op = Sequelize.Op;
+      await queryInterface.bulkDelete(options, {
+       [Op.or]: demoUsers
+      }, {}); 
+    } catch (error) {
+      console.error('Error in bulk deleting PMs:', error);
+    }
+  }
+};
