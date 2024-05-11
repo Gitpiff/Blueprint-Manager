0eb19566963cd515c0bb735425224a7c580f6142
Victor <viknavarro@gmail.com>
2024-05-11 18:14:41 +0100

    Login Form Page

---
+++ frontend/src/store/session.js
@@ -0,0 +1,70 @@
+import { csrfFetch } from './csrf';
+
+const SET_PROJECTMANAGER = 'session/setProjetManager';
+const REMOVE_PROJECTMANAGER = 'session/removeProjectManager';
+
+// Actions
+const setProjectManager = (projectManager) => {
+    return {
+        type: SET_PROJECTMANAGER,
+        payload: projectManager
+    };
+};
+
+const removeProjectManager = () => {
+    return {
+        type: REMOVE_PROJECTMANAGER
+    };
+};
+
+//Thunks
+export const login = (projectManager) => async (dispatch) => {
+    const { credential, password } = projectManager;
+    const response = await csrfFetch('api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    })
+
+    if(response.ok) {
+        const data = await response.json();
+        dispatch(setProjectManager(data.projectManager));
+        return response
+    } else {
+        const errors = await response.json();
+        return errors;
+    }
+}
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session", {
+        method: "DELETE"
+    })
+
+    dispatch(removeProjectManager())
+    return response
+}
+
+export const restoreProjectManager = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setProjectManager(data.projectManager));
+    return response;
+};
+
+const initialState = { projectManager: null };
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case SET_PROJECTMANAGER:
+        return { ...state, projectManager: action.payload };
+      case REMOVE_PROJECTMANAGER:
+        return { ...state, projectManager: null };
+      default:
+        return state;
+    }
+};
+  
+  export default sessionReducer;
